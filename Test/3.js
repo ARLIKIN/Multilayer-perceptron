60737e5ea6d2e1cc1980ce4a1b42925cf2cff4ff
ARLIKIN <nik5552000@mail.ru>
2020-11-26 20:08:22 +0300

    Подготовка к системе классов

--- Test/3.js
+++ Test/3.js
@@ -4,9 +4,39 @@ var Byid = function(id)
 }
 
 
+var str = '';
+
 var Test = function(id)
 {
     
     var thisText =  Byid(id).textContent;
     Byid('test11').textContent = thisText;
 }
+
+
+
+function showFile(input) {
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
+
+
+    reader.onload = function()
+    {
+        alert(reader.result);
+        str = reader.result;
+    }
+
+    reader.onerror = function() {
+        console.log(reader.error);
+      };
+  }
+
+
+  Byid('budlo').onclick = function()
+  {
+    var a = str.split(',')
+    alert(a[0]);
+    console.log(a)
+  }
+  
